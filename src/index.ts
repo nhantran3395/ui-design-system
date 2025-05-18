1a674bbf917cd100f6207f472933ae096e1e4c40
nhantran3395 <thanhnhan33.95@gmail.com>
2025-05-18 10:15:10 +0700

    Test new main and module settings in package.json

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { AlertDialog} from './AlertDialog';
-export { Button } from './Button';
-export { Checkbox } from './Checkbox';
-export { Dialog } from './Dialog';
-export { Table } from './Table';
-export { TextField } from './TextField';
-export { Tooltip } from './Tooltip';
